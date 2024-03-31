7085c478b3cfaf2dd5e086be485d2ba12421e57a
TDStrasser <43877585+TDStrasser@users.noreply.github.com>
2024-03-31 19:54:04 +1100

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,5 +1,11 @@
 const CACHE_NAME = `kookaterminal`;
 
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
   event.waitUntil((async () => {
